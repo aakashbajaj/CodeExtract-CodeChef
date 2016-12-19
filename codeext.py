8e4fe4a44fff41578b2f2090c3e28e3b2e215d1a
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2016-12-19 23:36:43 +0530

    Added HTML Source Request Suggestion

--- codeext.py
+++ codeext.py
@@ -5,3 +5,7 @@ except ImportError:
 	import urllib2
 	# For Python 2's
 
+# For The Final Code Page, Use the following request
+# import urllib.request
+# req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+# html_pg = urllib.request.urlopen(req).read()
