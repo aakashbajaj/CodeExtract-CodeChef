c1c438c7bf09a7e6f9f6917f8564f2480cb8e4ae
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2016-12-21 00:46:55 +0530

    Added UserName Reader

--- codeext.py
+++ codeext.py
@@ -9,3 +9,14 @@ except ImportError:
 # import urllib.request
 # req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 # html_pg = urllib.request.urlopen(req).read()
+
+usr_flg = 0
+
+while usr_flg:
+	try:
+		with open('username.txt','r') as uname:
+			user_name = str(uname.read().strip())
+			usr_flg = 1
+	except FileNotFoundError:
+		print("Can't Open File!")
+		usr_flg = 0
