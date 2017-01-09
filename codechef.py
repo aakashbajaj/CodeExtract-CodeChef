f5f0f9a429189887c033a128505f259535673b10
Aakash Bajaj <aakashbajaj5589@gmail.com>
2017-01-10 02:17:02 +0530

    Code Extarctor Function

--- codechef.py
+++ codechef.py
@@ -31,7 +31,7 @@ class Profile(object):
 		handle = self.handle
 		user_url = domain_url + "/users/" + handle
 		user_page = get_url_data(user_url)
-		soup = BeautifulSoup(user_page)
+		soup = BeautifulSoup(user_page,"lxml")
 		# Segregate problems table
 		sp = soup.find_all('table')[2]
 		
@@ -43,6 +43,34 @@ class Profile(object):
 
 		for s in n_soup.find_all('a'):
 			ques_link = (str(s.get('href'))).strip()
+			ques_link.
 			prob_list[s.text.strip()] = 
 
 		return prob_list
+
+	def extract_code(self, prob_code, sub_link):
+		"""Function Extracts the user submitted code from submission page"""
+		"""Return format @tuple (problem_code, code_lang, code_str)"""
+
+		domain_url = self.domain_url
+		code_page_url = domain_url + sub_link
+		code_page = get_url_data(code_page_url)
+		soup = BeautifulSoup(code_page,"lxml")
+
+		lang = (soup.find('pre')).get("class")[-1]
+
+		code = soup.find_all('ol')[-1]
+		
+		# in some cases code contains stray /xa0 character
+		# it needs to be removed
+		cod_str = str(code)
+		cod_str = cod_str.replace(u'\xa0',u' ')
+
+		# replacing </li> with </li>\n for pretty-printing
+		# then recreate a bs4 object
+
+		cod_str = cod_str.replace("</li>", "</li>\n")
+		code = BeautifulSoup(cod_str,"lxml")
+		code_str = code.text
+
+		return (prob_code, lang, code_str)
