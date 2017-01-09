935500a381ca51a8522590022d7caf6bdbf7232d
Aakash Bajaj <aakashbajaj5589@gmail.com>
2017-01-09 11:12:54 +0530

    Added location to file

--- filewrite.py
+++ filewrite.py
@@ -1,5 +1,5 @@
 
-def write_to_file(obj, fl_code, lang):
-	fl_name = fl_code + lang
+def write_to_file(obj, fl_location, fl_code, lang):
+	fl_name = fl_location + '/' + fl_code + '.' + lang
 	with open(fl_name,'w') as fl:
 		fl.write(obj)
