a5d20215e7d40eae64380dc3d7e527413d89936e
Aakash Bajaj <aakashbajaj5589@gmail.com>
2017-01-09 23:58:22 +0530

    URL Data Utility

---
+++ util.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen
+
+def get_url_data(url1):
+	req = urllib.request.Request(url1, headers={'User-Agent': 'Mozilla/5.0'})
+	html_pg = urllib.request.urlopen(req)
+	return html_pg
