947ee0c9051fcc7f7fbf9e741b538b77dff24a6d
Aakash Bajaj <aakashbajaj5589@gmail.com>
2017-01-13 02:37:02 +0530

    Filewrite feature in util

--- util.py
+++ util.py
@@ -5,3 +5,8 @@ def get_url_data(url1):
 	req = urllib.request.Request(url1, headers={'User-Agent': 'Mozilla/5.0'})
 	html_pg = urllib.request.urlopen(req)
 	return html_pg
+
+def write_to_file(code, location, prob_code, lang):
+	file_name = "./" + location + "/" + prob_code + "." + lang
+	with open(file_name,'w') as fl:
+		fl.write(code)
